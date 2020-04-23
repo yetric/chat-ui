9046bfda06c9928d2313a13987e1e6ea314a253a
Mattias Hising <hising@gmail.com>
2020-04-23 11:57:14 +0200

    Svelte support

---
+++ src/components/App.svelte
@@ -0,0 +1,12 @@
+<script>
+    export let name;
+</script>
+
+<style>
+    h1 {
+        text-align: center;
+    }
+</style>
+
+<h1>Hello {name}!</h1>
+<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi distinctio itaque mollitia nihil nisi quaerat saepe velit! Laboriosam nostrum velit veniam! Accusantium eligendi labore laborum non odio provident quia vero.</div>
--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("parser");
+import App from './components/App.svelte';
+
+const app = new App({
+    target: document.getElementById('chat'),
+    props: {
+        name: 'Chat UI',
+    },
+});
+
+export default app;
